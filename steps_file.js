3319b2b5858746ba24ca495f735632223721bbb8
cuongnguyenphu2005 <cuongnguyenphu2005@gmail.com>
2025-11-11 10:31:19 +0700

    Update tranfer to lms

--- steps_file.js
+++ steps_file.js
@@ -2,11 +2,19 @@ module.exports = function() {
   return actor({
     login(web = process.env.END_POINT,email = process.env.EMAIL_LOGIN, password = process.env.PASS_LOGIN) {
       this.amOnPage(web);
-      this.click('Đăng nhập');
-      this.fillField('Email', email);
-      this.fillField('Mật khẩu', password);
-      this.click('Đăng nhập');
-      this.waitForText('Đăng nhập thành công',10);
+      this.fillField('Wifi Username', email);
+      this.fillField('Password', password);
+      this.click('Log in');
+      this.waitForText('Hi, Cuong!',10);
+    },
+    
+    loginFail(web = process.env.END_POINT, invalidEmail = 'invalid@email.com', invalidPassword = 'wrongpassword') {
+      this.amOnPage(web);
+      this.fillField('Wifi Username', invalidEmail);
+      this.fillField('Password', invalidPassword);
+      this.click('Log in');
+      this.waitForText('Invalid login, please try again', 10);
     }
+    
   });
 }
--- tests/mainflow_test.js
+++ tests/mainflow_test.js
@@ -3,3 +3,7 @@ Feature('User Main Flow');
 Scenario('Đăng nhập thành công', async ({ I }) => {
   I.login();
 });
+
+Scenario('Đăng nhập thất bại', async ({ I }) => {
+  I.loginFail();
+});
